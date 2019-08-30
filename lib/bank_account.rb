0ffb8a0b993d8d431a3634f0f2fc85d4be06adfd
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-30 03:41:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,14 @@
 class BankAccount
-  attr_reader :name,
+  
+  attr_reader :name
+  attr_accessor :balance, :status
   
   def initialize(name)
     @name = name 
     @balance = 1000 
     @status = "open"
   end 
+  
+  
   
 end
